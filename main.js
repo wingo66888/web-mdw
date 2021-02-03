ecf4ecf49b5904133b46721fbe788189f6bb6288
wingo66888 <wingo66888@gmail.com>
2021-02-03 21:45:00 +0800

    連結分業

--- main.js
+++ main.js
@@ -6,5 +6,24 @@ $("#banner").slick({
 
 $("#works").slick({
     slidesToShow: 4,
-    slidesToScroll: 2
+    slidesToScroll: 2,
+    // 響應式 RWD
+    responsive: [
+        // 平板：小於 1025
+        {
+            breakpoint: 1025,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+            }
+        },
+        // 手機：小於 767
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
 });
